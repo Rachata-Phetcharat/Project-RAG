b80c4643de34d8be6f2f2fc596cbdd0d7a462703
Rachata Phetcharat <mr.bossvpmch59@gmail.com>
2026-02-05 20:41:38 +0700

    แก้ไข bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,25 +2,25 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  app: {
-    head: {
-      title: "KmutnbLM",
-      link: [{ rel: "icon", type: "image/x-icon", href: "/logo.png" }],
-    },
-  },
+  // app: {
+  //   head: {
+  //     title: "KmutnbLM",
+  //     link: [{ rel: "icon", type: "image/x-icon", href: "/logo.png" }],
+  //   },
+  // },
   modules: [
     "@nuxt/content",
     "@nuxt/image",
     "@nuxt/scripts",
     "@nuxt/ui",
     "@nuxt/eslint",
-    "@nuxt/hints",
+    // "@nuxt/hints",
     "@pinia/nuxt",
     "@nuxt/fonts",
   ],
   fonts: {
     families: [
-      { name: "Kanit", provider: "sans-serif" }, // เปลี่ยนเป็นชื่อฟอนต์ที่ชอบ
+      { name: "Kanit", provider: "google" }, // เปลี่ยนเป็นชื่อฟอนต์ที่ชอบ
     ],
   },
   css: ["~/assets/css/main.css"],
