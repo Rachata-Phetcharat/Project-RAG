8cde6016f95f05416bab135ac0d082fc23e74253
Rachata Phetcharat <mr.bossvpmch59@gmail.com>
2026-02-07 21:53:42 +0700

    อัพเดทชื่อเว็บ

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  // app: {
-  //   head: {
-  //     title: "KmutnbLM",
-  //     link: [{ rel: "icon", type: "image/x-icon", href: "/logo.png" }],
-  //   },
-  // },
+  app: {
+    head: {
+      title: "KmutnbLM",
+      link: [{ rel: "icon", type: "image/x-icon", href: "/logo.png" }],
+    },
+  },
   modules: [
     "@nuxt/content",
     "@nuxt/image",
